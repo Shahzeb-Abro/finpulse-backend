fe1caa0e17b6fe95cbc461363af7fd7a656ed3d1
shahzeb.abro <shahzeb.abro@adalfi.com>
2026-04-27 12:43:06 +0500

    Made sure testing APIs are only available when profile is either local or dev

--- src/main/java/com/finpulse/controller/TestController.java
+++ src/main/java/com/finpulse/controller/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-//@Profile(value = "local")
+@Profile({"local", "dev"})
 @RestController
 @RequestMapping("/v1/test")
 @RequiredArgsConstructor
